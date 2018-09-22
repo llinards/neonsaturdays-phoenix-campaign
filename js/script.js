8480dd1c6b928d68e97e4c63260aad055f76dac5
Linards <linards_lazdins@hotmail.com>
2018-09-22 22:26:27 +0300

    changed fonts, first preview

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("Connected!");
+$('a[href^="#"]').on('click', function (event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
