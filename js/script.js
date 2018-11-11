d15052dc436b83a988c602846137dd34c72e74ba
Linards Lazdiņš <linards_lazdins@hotmail.com>
2018-11-11 23:46:13 +0200

    to the top btn

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ $('a[href^="#"]').on('click', function (event) {
         }, 1000);
     }
 });
+
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 0
+    }, '300');
+});
